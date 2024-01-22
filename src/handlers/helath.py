b4e7ddbcd10f7c742c6b680f90301ac957292a66
Innokentii Kozlov <innokentiikozlov@gmail.com>
2024-01-22 13:41:29 +0200

    health check

---
+++ src/handlers/helath.py
@@ -0,0 +1,44 @@
+import json
+import tornado
+from tornado_swagger.model import register_swagger_model
+
+from config.logger import logger
+
+
+class Health(tornado.web.RequestHandler):
+
+    def initialize(self) -> None:
+        self.logger = logger
+
+    def get(self) -> None:
+        """
+        Description end-point
+
+        ---
+        tags:
+        - Health Check
+        summary: Check API
+        produces:
+        - application/json
+        responses:
+            200:
+                description: Ok
+                schema:
+                  $ref: '#/definitions/GetHealth'
+            400:
+                description: Bad request
+            404:
+                description: Not Found
+        """
+        self.write(json.dumps({"status": "ok"}))
+
+
+@register_swagger_model
+class GetHealth:
+    """
+    ---
+    type: object
+    properties:
+        status:
+            type: string
+    """
