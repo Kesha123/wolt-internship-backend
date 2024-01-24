b89cecea4a6b2ae58869c1b4b3d17c6973e614f9
Innokentii Kozlov <innokentiikozlov@gmail.com>
2024-01-24 12:12:16 +0200

    update constants module

--- src/utils/delivery_fee.py
+++ src/utils/delivery_fee.py
@@ -3,32 +3,12 @@ import sys
 from datetime import datetime
 
 try:
+    from constants.delivery_fee import *
     from api_types.calculator import DeliveryFeeRequest
 except:
     sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
+    from constants.delivery_fee import *
     from api_types.calculator import DeliveryFeeRequest
-
-
-SURCHARGE_BOTTOM = 10 * 100
-
-DISTANCE_INITIAL = 1000
-DISTANCE_ADDED = 500
-DISTANCE_FEE_INITIAL = 2 * 100
-DISTANCE_FEE_ADDED = 1 * 100
-
-ITEM_NUMBER_NO_CHARGE = 4
-ITEM_NUMBER_EXTRA_BULK = 12
-ITEM_FEE_INITIAL = 0
-ITEM_FEE_ADDED = 50
-ITEM_FEE_EXTRA_BULK = 1.2 * 100
-
-DELIVERY_FEE_LIMIT = 15 * 100
-FREE_DELIVERY_CART_VALUE = 200 * 100
-
-FRIDAY_DAY_NUMBER = 4
-FRIDAY_RUSH_TIME_START = 15
-FRIDAY_RUSH_TIME_END = 19
-FRIDAY_RUSH_MULTIPLIER = 1.2
 
 
 def small_order_surcharge(cart_value: int) -> int:
